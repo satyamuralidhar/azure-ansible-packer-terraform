41510322c5a69dbbc695173d61242fbede5972ad
satya <muralidhar.peddireddi@gmail.com>
2022-12-31 21:14:17 +0530

    git push

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,6 @@ echo $OSTYPE
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo yum update -y
     sudo yum install ansible -y
-    sudo subscription-manager repos --enable ansible-2.9-for-rhel-8-x86_64-rpms
-    sudo subscription-manager repos --enable rhel-7-server-ansible-2.9-rpms
 else 
     sudo apt -y update && sudo apt-get -y upgrade
     sudo apt -y install software-properties-common
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,8 +5,6 @@ echo $OSTYPE
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo yum update -y
     sudo yum install remove -y
-    sudo subscription-manager repos --disable ansible-2.9-for-rhel-8-x86_64-rpms
-    sudo subscription-manager repos --disable rhel-7-server-ansible-2.9-rpms
 else 
     sudo apt -y remove --purge ansible
     sudo apt-add-repository --remove ppa:ansible/ansible
